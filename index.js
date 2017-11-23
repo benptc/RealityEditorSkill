1e8fb1b593168dd1345349064c0f156e5cca4671
Ben Reynolds <breynolds@ptc.com>
2017-11-22 23:09:59 -0500

    added CORS

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ var express_app = require("express")();
 var express_server = require('http').Server(express_app);
 var io = require('socket.io')(express_server);
 
+express_app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 // app.express({ expressApp: express_app });
 
 app.express({
